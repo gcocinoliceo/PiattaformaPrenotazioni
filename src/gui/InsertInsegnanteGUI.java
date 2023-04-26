819279bde0202580d7c92c296b5f22c0eee884c1
gcocinoliceo <g.cocino@liceococito.it>
2023-04-26 12:45:19 +0200

    update

--- src/gui/InsertInsegnanteGUI.java
+++ src/gui/InsertInsegnanteGUI.java
@@ -46,6 +46,7 @@ public class InsertInsegnanteGUI extends JPanel implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		
 	}
 	
     public JButton getButton() {
